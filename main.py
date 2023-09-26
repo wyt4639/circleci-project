0c62ad1d02d57e1577a5f47468629e8e95308877
Kathy <wyt4639@autuni.ac.nz>
2023-09-27 11:58:40 +1300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def to_upper(name):
+    return name.upper()
+
+def say_hello(name):
+    print(f'Hello,{name}')
+    if __name__=='__main__':
+        name='Tony'
+        say_hello(name)
+        up = to_upper(name)
+        print(up) 
